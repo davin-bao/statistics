7815d7a5950076e4c746a7fcd49eff5255d77e27
davin.bao <davin.bao@gmail.com>
2014-05-13 23:00:08 +0800

    add lang/zh_CN/statistics.php

--- src/lang/zh_CN/statistics.php
+++ src/lang/zh_CN/statistics.php
@@ -7,4 +7,25 @@
  */
 
 return array(
+    'statistics' => '统计',
+
+    'name' => '名称',
+    'column_names' => '结果集列标题',
+    'type' => '类型',
+
+
+
+    'name' => '名称',
+    'follower' => '参与者',
+    'select_all' => '全选',
+    'create' => '创建',
+    'edit' => '修改',
+    'delete' => '删除',
+
+    'success' => '成功',
+    'saved' => '已保存',
+    'general_info' => '基本信息',
+    'does_not_exist' => '该信息不存在',
+    'action' => '操作',
+    'failed_unknown' => '失败',
 );
